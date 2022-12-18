0ec00dae112adcb82f9aac9bb971f970b93e4b23
maroozrokh <mahsaroozrokh@yahoo.com>
2022-12-18 14:36:29 +0330

    first pro

---
+++ bin/first_class.dart
@@ -0,0 +1,5 @@
+import 'package:first_class/first_class.dart' as first_class;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_class.calculate()}!');
+}
---
+++ test/first_class_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_class/first_class.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
